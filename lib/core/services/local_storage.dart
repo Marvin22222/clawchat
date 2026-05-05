f570b304b2566601dab701e85d3f8733537b2cd0
Marvin Schwab <marvin@clawchat.dev>
2026-05-05 15:52:45 +0000

    fix(local_storage): fix incorrect import path

--- lib/core/services/local_storage.dart
+++ lib/core/services/local_storage.dart
@@ -1,5 +1,5 @@
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../core/constants/app_config.dart';
+import '../constants/app_config.dart';
 
 class LocalStorage {
   static SharedPreferences? _prefs;
